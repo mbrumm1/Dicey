4f3fb47722a41dd22cec7b64106ecf2c0153cd17
Michael Brummerhop <michaelb@campusfederal.org>
2023-12-15 19:37:50 -0800

    Add project files.

---
+++ src/Dicey.Web/Models/DiceSet.cs
@@ -0,0 +1,7 @@
+﻿namespace Dicey.Web.Models;
+
+public class DiceSet
+{
+    public string Name { get; set; } = string.Empty;
+    public List<Die> Dice { get; set; } = new List<Die>();
+}
---
+++ src/Dicey.Web/Models/Die.cs
@@ -0,0 +1,12 @@
+﻿namespace Dicey.Web.Models;
+
+public class Die
+{
+    public int Sides { get; set; }
+    public int Value { get; set; }
+
+    public void Reset()
+    {
+        Value = Sides;
+    }
+}
